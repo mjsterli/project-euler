8312194490460de9e9216c67377f73a9c492ed9a
mjsterli <mjsterli@gmail.com>
2021-10-27 22:15:20 -0500

    Added solution for problem 4

---
+++ 004_LargestPalindromeProduct/LargestPalindromeProduct.js
@@ -0,0 +1,29 @@
+let s = 999, largestPalindrome = 0;
+
+while(s > 0){
+  e = s;
+  do{
+    product = s * e;
+    if(IsAPalindrome(product.toString()) && product > largestPalindrome)
+      largestPalindrome = product;
+
+    e--;
+  }while(e > 0)
+
+  s--;
+}
+
+console.log(`The largest palindrome is: ${largestPalindrome.toString()}`);
+
+function IsAPalindrome(number){
+  let s = 0, e = number.length - 1, isAPalindrome = true;
+
+  while(isAPalindrome && s < e){
+    isAPalindrome &= number.charAt(s) === number.charAt(e);
+
+    s++;
+    e--;
+  }
+
+  return isAPalindrome;
+}
